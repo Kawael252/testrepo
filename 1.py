5f48e7380353efbefcd0f8f385c325559db5f8a6
kawael252 <L.G.Luedicke2@gmail.com>
2019-10-07 13:33:58 -0400

    add calc and elif operation

--- 1.py
+++ 1.py
@@ -1,5 +1,23 @@
-print("moin moin")
-name = input("name bidde: ")
+print("Moin Moin")
+name = input("Name bidde: ")
 print("Das merke ich mir", name)
-zeit = int(input())
-print(zeit)
+
+anzahl = int(input("Wie viel Zigaretten rauchst Du am Tag? "))
+anzahlmonat = anzahl * 30
+anzahljahr = anzahl * 365
+print("Das sind", anzahlmonat, "im Monat", "und", anzahljahr, "im Jahr,")
+gelogen = 10
+uebertrieben = 30
+
+if anzahl <= gelogen:
+    print("erzähl mir kein Scheiss, Du rauchst mehr!")
+    print(name, "erzähl meinem Programm keinen Unsinn.")
+
+elif anzahl <= uebertrieben:
+    print("so hätte ich Dich auch eingeschätzt.")
+    print("Weniger rauchen", name,
+          "weil eine Zigarette so viel Feinstaub erzeugt wie eine 10km Autofahrt.")
+
+elif anzahl > uebertrieben:
+    print("Deeeega ganz schön viel!")
+    print("In jedem Fall weniger rauchen", name)
